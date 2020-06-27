327d9d32eafc5aa7891a66c659b32eee725645c9
rsena <rsenaulgur@gmail.com>
2020-06-27 14:10:28 -0400

    mentoring added

--- src/Sena/Class1.java
+++ src/Sena/Class1.java
@@ -1,4 +1,15 @@
 package Sena;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Class1 {
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver", "/Users/RefiaSena/Downloads/chromedriverV83");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///Users/RefiaSena/Documents/SeleniumMentoring/src/Sena/M1_Dropdowns/ref/SignUpFormDropdowns.html");
+
+    }
 }
