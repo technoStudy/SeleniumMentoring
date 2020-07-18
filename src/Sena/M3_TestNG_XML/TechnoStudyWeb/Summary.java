b6efd22fbb64b6cce25e0ead93d00f69d0c74df1
rsena <rsenaulgur@gmail.com>
2020-07-18 17:48:30 -0400

    mentoring added

---
+++ src/Sena/M3_TestNG_XML/TechnoStudyWeb/Summary.java
@@ -0,0 +1,13 @@
+package Sena.M3_TestNG_XML.TechnoStudyWeb;
+
+public class Summary {
+
+    // TESTNG XML FILE
+    // Why do we need it?
+    // - to run couple classes / tests at once
+    // - save time
+    // - parallel testing
+    // - grouping tests
+
+    // suite -> tests -> test -> classes -> class
+}
